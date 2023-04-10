34a3ebc4484534b1d076b5bc43314b62973351db
zhanghong27 <130319076+zhanghong27@users.noreply.github.com>
2023-04-09 21:06:18 -0400

    Delete forage-lyft-starter-repo directory

--- forage-lyft-starter-repo/battery_base.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Battery(ABC):
-    @abstractmethod
-    def needs_service(self):
-        pass
--- forage-lyft-starter-repo/serviceable.py
+++
@@ -1,7 +0,0 @@
-from car import Car
-
-from abc import ABC
-
-class Serviceable(ABC):
-    def needs_service(self):
-        pass
